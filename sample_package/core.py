ff0cfb53b4344cfd06de44a5453bfe0a7eabab03
Tobias Rohrer <tobias.rohrer@outlook.com>
2022-10-21 11:03:11 +0200

    added sphinx to document the sample project

--- sample_package/core.py
+++ sample_package/core.py
@@ -1,11 +1,37 @@
 import numpy as np
 
+
 class Algorithm:
-    def __init__(self, parameter_a):
+    """
+    An Algorithm which is initialized with parameter_a. Using `calc()` you can add any int to that parameter.
+
+    :parameter_a: Any int which gets added
+    :type parameter_a: int
+
+    """
+    def __init__(self, parameter_a: int):
+        """
+        Constructor initializing parameter_a with the given value.
+
+        :param parameter_a:
+        :type parameter_a: int
+        :returns: Nothing
+        :rtype: None
+
+        """
         self.parameter_a = parameter_a
         pass
 
-    def calc(self, parameter_b):
+    def calc(self, parameter_b: int) -> int:
+        """
+        Add a parameter to the internal one.
+
+        :param parameter_b: parameter to add
+        :type parameter_b: int
+        :returns: the result
+        :rtype: int
+
+        """
         print("calculating something inside the package")
         result = self.parameter_a + parameter_b
         return result
